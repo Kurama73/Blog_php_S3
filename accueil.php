9ea334f77fee5b9e61f09db74c72befa98418b3f
tom.frumy <tom.frumy@etu.univ-lyon1.fr>
2024-10-14 17:28:36 +0200

    correction

--- accueil.php
+++ accueil.php
@@ -5,7 +5,7 @@
     require 'pdo.php';
     
     // Récupération des données depuis la BD
-    $stmt = $dbh->prepare("SELECT * FROM partie");
+    $stmt = $dbh->prepare("");
     $stmt->execute();
     $parties = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -15,7 +15,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jeu du Pendu</title>
+    <title>Blog</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
