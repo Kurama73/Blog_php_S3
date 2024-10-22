7ecea1ef06f467cb6287499c68f3ddf46beeec16
Koenig Thomas <thomas.koenig@etu.univ-lyon1.fr>
2024-10-22 16:55:59 +0200

    boutton deco

--- src/header.php
+++ src/header.php
@@ -2,6 +2,18 @@
 session_start();
 require_once('pdo.php');
 
+if (!(basename($_SERVER['PHP_SELF']) == "index.php")) {
+    require ('redirection.php');
+}
+
+
+if (isset($_POST['log-out'])) {
+
+    $_SESSION["isConnected"] = false;
+    header("Location: index.php");
+    exit;
+}
+
 ?>
 
 <!doctype html>
@@ -19,6 +31,14 @@ require_once('pdo.php');
 
 <header class="px-5 py-7 shadow-md bg-gray-400">
     <h1 class="text-2xl font-bold"><a href="home.php">Blog.kpf</a></h1>
+
+
+    <form method="post">
+
+        <input type="hidden" name="log-out"/>
+        <input type="submit" value="Deconnexion">
+
+    </form>
 </header>
 
 </body>
