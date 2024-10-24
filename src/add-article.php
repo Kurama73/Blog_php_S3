8027174c6ce4687e430560cf3622280d268f997c
tom.frumy <tom.frumy@etu.univ-lyon1.fr>
2024-10-24 16:59:21 +0200

    cssadmin

--- src/add-article.php
+++ src/add-article.php
@@ -49,7 +49,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Blog - add article</title>
-        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="bg-gray-100 flex justify-center items-center min-h-screen">
 
